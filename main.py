14d49a2bc53c3983017441d8cad44fa096bf3dd5
Akhmad Yudha Mahardika <akhmad.yudha.ketiga@gmail.com>
2026-07-22 09:54:29 +0700

    Using pydantic data format

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from dotenv import load_dotenv
 
+from pydantic import BaseModel, Field
+from typing import List
+
 load_dotenv()
 from langchain.agents import create_agent
 from langchain.tools import tool
@@ -7,10 +10,21 @@ from langchain_core.messages import HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_tavily import TavilySearch
 
+class Source(BaseModel):
+  """Schema for a source used by the agent"""
+
+  url:str = Field(description="The Url of the source")
+
+class AgentResponse(BaseModel):
+    """SChema for agent response with answer and sources"""
+
+    answer: str = Field(description="The agent's answer to the query")
+    sources: List[Source] = Field(default_factory=list, description="List of sources used to generate the answer")
+
 
 llm = ChatGoogleGenerativeAI(model='gemini-3.1-flash-lite')
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 def main():
   print("hello")
